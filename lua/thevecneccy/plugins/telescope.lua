b048bc18b34a83b132314f321d1a684b4f9d832b
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-10-10 01:06:53 +0500

    changed keymaps for telescope

--- lua/thevecneccy/plugins/telescope.lua
+++ lua/thevecneccy/plugins/telescope.lua
@@ -3,9 +3,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>ff', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader><leader>', builtin.help_tags, { desc = 'Telescope help tags' })
     end
 }
