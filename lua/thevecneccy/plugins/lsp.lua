c12f7c49336df0499a699d7c213737ae41335f76
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-10-10 05:24:03 +0500

    LSP finally

---
+++ lua/thevecneccy/plugins/lsp.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            -- configure all the servers here
+            vim.lsp.config['luals'] = {
+                cmd = { 'lua-language-server' },
+                filetypes = { 'lua' },
+                root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+            }
+            vim.lsp.enable('luals')
+            vim.lsp.config['gopls'] = {
+                cmd = { 'gopls' },
+                filetypes = { 'go' },
+                root_markers = { { 'go.mod' }, '.git' },
+            }
+            vim.lsp.enable('gopls')
+
+            -- some keybindings to help me go kaboom
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, silent = true })
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
+
+            -- to see errors
+            vim.keymap.set('n', '<leader>k', function()
+                vim.diagnostic.open_float({ scope = 'line' })
+            end, { noremap = true, silent = true })
+
+            -- to navigate errors
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true, buffer = bufnr })
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = true, buffer = bufnr })
+        end,
+    },
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    }
+}
