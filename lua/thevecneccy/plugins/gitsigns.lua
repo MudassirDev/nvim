92ce18c186ea3d629f4fc622069fe7717519b3a1
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-10-11 15:05:52 +0500

    added fugitive

--- lua/thevecneccy/plugins/gitsigns.lua
+++ lua/thevecneccy/plugins/gitsigns.lua
@@ -1,6 +1,15 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup()
-    end
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+            vim.keymap.set("n", "<leader>g", ":Gitsigns preview_hunk<CR>", {})
+        end
+    },
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end
+    },
 }
