ae96bf7d9f0ce2e084ce5ca475350220b450bf41
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-10-15 14:33:13 +0500

    added lsp rename

--- lua/thevecneccy/plugins/lsp.lua
+++ lua/thevecneccy/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
                 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
                 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "<leader>ss", vim.lsp.buf.rename, opts)
                 vim.keymap.set("n", "<leader>k", function()
                     vim.diagnostic.open_float()
                 end,opts)
