e743f31436fec952af22b2002a520413350bc87a
Mudassir Bilal <mughalmudassir966@gmail.com>
2026-01-07 11:17:48 +0500

    changed theme

--- lua/thevecneccy/plugins/rose-pine.lua
+++ lua/thevecneccy/plugins/rose-pine.lua
@@ -1,16 +1,26 @@
+-- return {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     config = function()
+--         require("rose-pine").setup({
+--             variant = "main",
+--             palette = {
+--                 main = {
+--                     base = '#000000',
+--                 },
+--             },
+--         })
+--         vim.cmd("colorscheme rose-pine")
+--         vim.api.nvim_set_hl(0, "NormalFloat", {bg = "#000000"})
+--     end
+-- }
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-        require("rose-pine").setup({
-            variant = "main",
-            palette = {
-                main = {
-                    base = '#000000',
-                },
-            },
-        })
-        vim.cmd("colorscheme rose-pine")
-        vim.api.nvim_set_hl(0, "NormalFloat", {bg = "#000000"})
-    end
+    "tiagovla/tokyodark.nvim",
+    opts = {
+        -- custom options here
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts) -- calling setup is optional
+        vim.cmd [[colorscheme tokyodark]]
+    end,
 }
